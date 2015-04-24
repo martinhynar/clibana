7a1d6adf61321405131ef693f17f06edf971260b
martinhynar <martin.hynar@gmail.com>
2015-04-24 09:33:40 +0200

    a place where I play with stuff

--- src/cljx/play_with_clibana.clj
+++ src/cljx/play_with_clibana.clj
@@ -1,6 +1,8 @@
 (ns play-with-clibana
   (:require [clibana.search :as cs]
             [clibana.query :as cq]
+            [clibana.url :as cu]
+            [clibana.visualization :as cv]
             [clojurewerkz.elastisch.native :as es]
             [clojurewerkz.elastisch.native.index :as esi]
             [clojurewerkz.elastisch.query :as esq]
@@ -12,19 +14,53 @@
   (reset! client (es/connect [["127.0.0.1" 9300]]
                              {"cluster.name" "elasticsearch"})))
 
+
+(defn delete-all-kibana-documents []
+  (esd/delete-by-query-across-all-types @client ".kibana" (esq/match-all)))
+
+(defn delete-all-kibana-search-documents []
+  (esd/delete-by-query @client ".kibana" "search" (esq/match-all)))
+
+
 (defn store-search [search-doc]
   (esd/create @client ".kibana" "search" search-doc))
-
-(defn delete-all-search-documents []
-  (esd/delete-by-query @client ".kibana" "search" (esq/match-all)))
 
 
 
 (defn example []
-  (cs/search "Test Search"
-             (cs/with-description "Match all documents with service foo.")
-             (cs/with-columns "service" "metric")
-             (cs/with-sort "metric" :asc)
-             (cs/with-index-pattern "test")
-             (cq/with-terms "service" "foo")
-             ))
+  )
+
+
+
+
+(comment
+  ;http:// localhost:5601/# / dashboard/Dash1?
+  ;_g= (refreshInterval: (display:'5%20seconds', section:1, value:5000), time: (from:now-15m, mode:quick, to:now))
+  ;&
+  ;_a= (filters:! (), panels:! ((col:1, id:Viz1, row:1, size_x:3, size_y:2, type:visualization)),, title:Dash1)
+  )
+
+(defn url-example []
+  (cu/dashboard-url
+    (cu/with-host " http " " localhost " 5601)
+    (cu/with-title " Dashboard Title ")
+    (cu/with-time-relative 5 :hour)
+    (cu/with-id " Dashboard ID ")                           ; This shall be mandatory
+    (cu/with-query (clibana.query/with-terms :field " term "))
+    )
+  )
+
+(def search (cs/search "A search"))
+
+(defn visualization-example []
+  (cv/visualization "An Example"
+                 (cv/with-description "This visualization shows ...")
+                 (cv/with-options :encode-json? false)
+                 (cv/with-bar-chart
+                   (cv/chart-with-parameter :mode :stacked)
+                   (cv/chart-with-parameter :spyPerPage 10)
+                   (cv/chart-with-Y-aggregation :max :field "metric")
+                   (cv/chart-with-X-aggregation :date-histogram "@timestamp")
+                   (cv/chart-with-X-aggregation :terms "service"))
+                 (cv/with-saved-search search))
+  )
